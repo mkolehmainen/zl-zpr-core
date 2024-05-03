e1be398cdcd805f92a3eaa40d66563df76e93c42
Mathias Kolehmainen <mkolehmainen@appliedinvention.com>
2024-05-03 16:10:12 -0400

    unit tests

--- adapter/cd/src/cd/zpr.rs
+++ adapter/cd/src/cd/zpr.rs
@@ -1,13 +1,15 @@
-use std::fs;
-use std::io::{BufReader, Error, ErrorKind, Read};
+use std::{
+    fs,
+    io::{BufReader, Error, ErrorKind, Read},
+    time::Instant,
+    sync::{Arc, Mutex},
+};
 
-use std::time::Instant;
 
-use std::sync::{Arc, Mutex};
 
 use serde::Deserialize;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum ConfigState {
     Connecting,
     Connected(Instant),
@@ -65,6 +67,8 @@ pub fn load_configuration(path: &str) -> Result<Configuration, std::io::Error> {
     c.path_name = path.to_string();
     Ok(c)
 }
+
+
 
 #[derive(Debug, Clone)]
 pub struct Zpr {
@@ -201,3 +205,196 @@ impl Zpr {
     }
 
 }
+
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::env;    
+    use std::time::{SystemTime, UNIX_EPOCH};
+    use rand::Rng;
+
+
+    struct TempTomlFile {
+        path: String,
+    }
+
+    impl Drop for TempTomlFile {
+        fn drop(&mut self) {
+            let _ = fs::remove_file(&self.path);
+        }
+    }
+
+    impl TempTomlFile {
+        fn new(contents: &str) -> TempTomlFile {
+            let mut rng = rand::thread_rng();
+            let tstamp = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis();
+            let dir = env::temp_dir();
+            let num: u32 = rng.gen();
+            let path = dir.join(format!("org_zpr_cd_test_{}_{}.toml", num, tstamp));
+            fs::write(&path, contents).expect("Unable to write file");
+            TempTomlFile {
+                path: path.to_str().unwrap().to_string(),
+            }
+        }
+
+        fn get_path(&self) -> &str {
+            self.path.as_str()
+        }
+    }
+
+    
+
+    #[test]    
+    fn test_load_configuration() {
+        let toml_txt = r#"
+            [profile]
+            name = "test"
+            [dock]
+            host_or_ip = "localhost"
+            startup_port = 2242
+            [adapter]
+            #blank
+        "#;
+        let tmpfile = TempTomlFile::new(toml_txt);
+        let c = load_configuration(tmpfile.get_path());
+        if let Err(e) = c {
+            panic!("Error loading configuration: {}", e);
+        }
+        assert!(c.is_ok());
+        let c = c.unwrap();
+        assert_eq!(c.profile.name, "test");
+        assert_eq!(c.get_name(), "test");
+        assert_eq!(c.dock.host_or_ip, "localhost");
+        assert_eq!(c.dock.startup_port, 2242);
+        assert_eq!(c.adapter.private_key, None);
+    }
+
+    #[test]
+    fn test_add_configuration() {
+        let toml_txt = r#"
+            [profile]
+            name = "test"
+            [dock]
+            host_or_ip = "localhost"
+            startup_port = 2242
+            [adapter]
+            #blank
+        "#;
+        let tmpfile = TempTomlFile::new(toml_txt);
+        let c = load_configuration(tmpfile.get_path());
+        assert!(c.is_ok());
+
+        let conf = c.unwrap();
+        let zpr = Zpr::new();
+
+        let mut stats = zpr.get_status();
+        assert_eq!(stats.len(), 0);
+
+        let r = zpr.add_configuration(conf);
+        if let Err(e) = r {
+            panic!("Error adding configuration to Zpr: {}", e);
+        }
+        assert!(r.is_ok());
+
+        stats = zpr.get_status();
+        assert_eq!(stats.len(), 1);
+        assert_eq!(stats[0].0, "test");
+        assert_eq!(stats[0].1, "localhost");        
+        assert_eq!(stats[0].2, "disconnected");                
+    }
+
+    #[test]
+    fn test_cannot_have_duplicate_name() {
+        let toml_txt = r#"
+            [profile]
+            name = "test"
+            [dock]
+            host_or_ip = "localhost"
+            startup_port = 2242
+            [adapter]
+            #blank
+        "#;
+        let tmpfile1 = TempTomlFile::new(toml_txt);
+        let c = load_configuration(tmpfile1.get_path());
+        assert!(c.is_ok());
+        let conf = c.unwrap();
+
+        let zpr = Zpr::new();
+
+        let r = zpr.add_configuration(conf);
+        if let Err(e) = r {
+            panic!("Error adding configuration to Zpr: {}", e);
+        }
+        assert!(r.is_ok());
+
+
+        let toml_txt = r#"
+            [profile]
+            name = "test"
+            [dock]
+            host_or_ip = "anotherlocalhost"
+            startup_port = 2243
+            [adapter]
+            #blank
+        "#;
+        let tmpfile2 = TempTomlFile::new(toml_txt);
+        let c = load_configuration(tmpfile2.get_path());
+        assert!(c.is_ok());
+        let conf2 = c.unwrap();
+        let r = zpr.add_configuration(conf2);
+        assert!(r.is_err());
+        let e = r.unwrap_err();
+        assert!( e.to_string().contains("Configuration with name test already exists"));
+    }
+
+    #[test]
+    fn test_empty_zpr_no_crash() {
+        let zpr = Zpr::new();
+        let stats = zpr.get_status();
+        assert_eq!(stats.len(), 0);
+        assert!(zpr.get_configuration_state("foo").is_none());
+        let r = zpr.set_status("foo", ConfigState::Disconnected);
+        assert!(r.is_err());
+        let e = r.unwrap_err();
+        assert!( e.to_string().contains("Configuration with name foo not found"));
+    }
+
+    #[test]
+    fn test_set_state() {
+        let toml_txt = r#"
+            [profile]
+            name = "test"
+            [dock]
+            host_or_ip = "localhost"
+            startup_port = 2242
+            [adapter]
+            #blank
+        "#;
+        let tmpfile = TempTomlFile::new(toml_txt);
+        let c = load_configuration(tmpfile.get_path());
+        assert!(c.is_ok());
+        let conf = c.unwrap();
+        let zpr = Zpr::new();
+        let r = zpr.add_configuration(conf);
+        assert!(r.is_ok());
+
+        let state = zpr.get_configuration_state("test");
+        assert!(state.is_some());
+        assert_eq!(state.unwrap(), ConfigState::Disconnected);
+
+        let r = zpr.set_status("test", ConfigState::Connecting);
+        assert!(r.is_ok());
+        let state = zpr.get_configuration_state("test");
+        assert!(state.is_some());
+        assert_eq!(state.unwrap(), ConfigState::Connecting);
+
+        let r = zpr.set_status("test", ConfigState::Connected(Instant::now()));
+        assert!(r.is_ok());
+        let state = zpr.get_configuration_state("test");
+        assert!(state.is_some());
+        assert!(matches!(state.unwrap(), ConfigState::Connected(_)));
+    }
+}
+
