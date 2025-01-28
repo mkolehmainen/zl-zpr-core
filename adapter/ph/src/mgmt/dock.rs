a0771a7f69bfcf2d647c27f9bf5ee79efe4be7d5
svjaffer <75515618+svjaffer@users.noreply.github.com>
2025-01-28 12:57:49 -0500

    Add entries in the forwarding table based on visa destination (#655)
    * Update integration tests to allow 3 adapters

--- adapter/ph/src/mgmt/dock.rs
+++ adapter/ph/src/mgmt/dock.rs
@@ -2,6 +2,7 @@ use crate::assembly::{AddRouteError, Assembly};
 use crate::counters::CounterType;
 use crate::defs::FiveTuple;
 use crate::logging::targets::FLOW_MGMT;
+use crate::net_defs::IpAddress;
 use crate::special_peers;
 use libnode::{vsapi, vsconn};
 use std::num::NonZero;
@@ -9,11 +10,14 @@ use std::sync::Arc;
 use thiserror::Error;
 use tracing::*;
 use zpr::{self, LinkId, StreamId};
+use zpr_ext::std::num::NonZeroExt;
 
 #[derive(Debug, Error)]
 pub enum BindAgentAddressError {
     #[error("policy error")]
     PolicyError,
+    #[error("parse error: {0}")]
+    ParseError(String),
     #[error("adding route failed: {0}")]
     AddRouteError(AddRouteError),
 }
@@ -37,15 +41,21 @@ pub async fn bind_agent_address(
             return Err(BindAgentAddressError::PolicyError);
         }
     } else {
-        // HACK: for now, we assume a visa which forwards through to the other adapter
-        // AND ALSO we manually issue a bind request out to that adapter
-        let Some(proposed_egress_link_id) = asm.hack_default_policy(ingress_link_id) else {
+        if ingress_link_id.get() == zpr::LOCAL_AGENT_LINK_ID {
+            // Reject packets from the local agent.
+            // (Packets destined to the Visa Service Adapter fall under special-peer policy.)
             return Err(BindAgentAddressError::PolicyError);
-        };
+        }
 
-        if proposed_egress_link_id.get() == zpr::LOCAL_AGENT_LINK_ID {
+        let visa_server_id = asm
+            .peer_table
+            .lookup_special_peer(crate::special_peers::SpecialPeerName::VisaServiceAdapter)
+            .unwrap_or_zero();
+        if ingress_link_id.get() == visa_server_id {
             // VERY HACK
-            egress_link_id = proposed_egress_link_id;
+            // Unconditionally accept traffic from the Visa Service Adapter;
+            // forward it to our local agent.
+            egress_link_id = NonZero::new(zpr::LOCAL_AGENT_LINK_ID).unwrap();
         } else {
             let visa_req = vsconn::VisaRequest {
                 source_tether_addr: five_tuple.src_address.into(),
@@ -57,13 +67,37 @@ pub async fn bind_agent_address(
             match asm.vsconn.as_ref().unwrap().request_visa(visa_req).await {
                 Ok(vsapi::VisaResponse {
                     status: Some(vsapi::StatusCode::SUCCESS),
+                    visa,
                     ..
                 }) => {
+                    // for now, just pull the destination address tether to set up forwarding
+                    let Some(octets) = visa.unwrap().visa.unwrap().dest else {
+                        asm.counters[CounterType::VisaRequestError].increment();
+                        error!(target: FLOW_MGMT, "visa request error: Could not parse visa");
+                        return Err(BindAgentAddressError::ParseError(
+                            "Could not parse visa".into(),
+                        ));
+                    };
+                    let Ok(addr) = IpAddress::try_from(octets) else {
+                        asm.counters[CounterType::VisaRequestError].increment();
+                        return Err(BindAgentAddressError::ParseError(
+                            "Could not parse visa dest address".into(),
+                        ));
+                    };
+                    match asm.find_egress_link(addr) {
+                        Some(link_id) => egress_link_id = link_id,
+                        None => {
+                            asm.counters[CounterType::VisaRequestError].increment();
+                            return Err(BindAgentAddressError::ParseError(
+                                "Could not parse visa dest address".into(),
+                            ));
+                        }
+                    }
+                    asm.counters[CounterType::VisaRequestSuccess].increment();
                     debug!(
                         target: FLOW_MGMT,
-                        "visa request succeeds, egress_link_id = {proposed_egress_link_id}"
+                        "visa request succeeds, egress_link_id = {egress_link_id}"
                     );
-                    egress_link_id = proposed_egress_link_id;
                 }
 
                 Ok(resp) => {
