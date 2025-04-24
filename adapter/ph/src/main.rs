6da2483698ac05f6b977b843dfc64789e86dc8e4
mkolehmainen <166627049+mkolehmainen@users.noreply.github.com>
2025-04-24 16:45:02 -0400

    refactor some essentially global PKI functions into pki module (#814)

--- adapter/ph/src/main.rs
+++ adapter/ph/src/main.rs
@@ -48,6 +48,7 @@ mod packet_queue;
 mod packet_steering;
 mod pcap_writer;
 mod peer_table;
+mod pki;
 mod queues;
 mod rcu;
 mod sample_ring;
@@ -76,6 +77,7 @@ use flow_control::FlowControl;
 use km_multiplexor::KmState;
 use km_noise::NoiseKeypair;
 use logging::targets::STARTUP;
+use pki::{load_cert, load_noise_public_key};
 use queues::*;
 use sys::ZprTun;
 use tun_ctl::TunCtl;
@@ -146,15 +148,14 @@ fn main() -> ExitCode {
         peer_noise_keypair = None;
         self_noise_keypair = Some(NoiseKeypair::new(private_key));
     } else {
-        let public_key = match km_cert_exchange::load_public_key(&Path::new(
-            &config.node_public_key_file.unwrap(),
-        )) {
-            Ok(key) => key,
-            Err(e) => {
-                error!(target: STARTUP, "failed to load node public key file: {e:?}");
-                return ExitCode::FAILURE;
-            }
-        };
+        let public_key =
+            match load_noise_public_key(&Path::new(&config.node_public_key_file.unwrap())) {
+                Ok(key) => key,
+                Err(e) => {
+                    error!(target: STARTUP, "failed to load node public key file: {e:?}");
+                    return ExitCode::FAILURE;
+                }
+            };
         peer_noise_keypair = Some(NoiseKeypair {
             public: public_key,
             private: [0u8; 32], // unknown
@@ -300,8 +301,7 @@ fn main() -> ExitCode {
     let vs_outq;
 
     if ph_mode == PhMode::Node {
-        let node_cert = km_cert_exchange::load_cert(&config.certificate_file)
-            .expect("unable to read certificate");
+        let node_cert = load_cert(&config.certificate_file).expect("unable to read certificate");
 
         let node_name = node_cert
             .subject_name()
